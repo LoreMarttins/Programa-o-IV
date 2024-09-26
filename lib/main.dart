b3b44fa94900383a870c0cb62eb119a53e97dfa1
Lore <109928731+LoreMarttins@users.noreply.github.com>
2024-09-25 22:29:04 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import './my_card.dart';
+
+void main() {
+  runApp(const MyCardApp());
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import './info_card.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color.fromARGB(255, 0, 150, 105),
+        width: double.infinity,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 25.0,
+              backgroundImage: AssetImage('assets/images/avatar.jpg'),
+            ),
+            Text(
+              'Fulana da Silva',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            Text(
+              'DESENVOLVEDOR FLUTTER',
+              style: TextStyle(
+                color: Colors.white54,
+                fontSize: 8.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Divider(
+              thickness: 0.25,
+              indent: 50.0,
+              endIndent: 50.0,
+              color: Colors.white54,
+            ),
+            InfoCard(
+              icon: Icons.phone,
+              text: '+55 86 99888-1122',
+            ),
+            InfoCard(
+              icon: Icons.email,
+              text: 'fulano.anzois@email.com',
+            ),
+            // InfoCard(
+            //   icon: Icons.star,
+            //   text: 'Av da Pesca, 1',
+            //   color: Colors.red,
+            //   backgroundColor: Colors.grey,
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
